dd864ea763eebaa83475bbd47fc835f97e5c7f56
Kento75 <kentonet75@gmail.com>
2018-06-20 06:22:17 +0900

    サイトコンポーネントの分割

--- wp-content/themes/pacific/header.php
+++ wp-content/themes/pacific/header.php
@@ -1,71 +1,61 @@
-<?php
-/**
- * The header for our theme
- *
- * This is the template that displays all of the <head> section and everything up until <div id="content">
- *
- * @link https://developer.wordpress.org/themes/basics/template-files/#template-partials
- *
- * @package Pacific
- */
-
-?><!doctype html>
-<html <?php language_attributes(); ?> class="no-js no-svg">
+<!DOCTYPE HTML>
+<html dir="ltr" lang="ja">
 <head>
-<meta charset="<?php bloginfo( 'charset' ); ?>">
-<meta name="viewport" content="width=device-width, initial-scale=1">
-<link rel="profile" href="http://gmpg.org/xfn/11">
-
-<?php wp_head(); ?>
+    <meta http-equiv="Content-Type" content="text/html; charset=UTF-8">
+    <title><?php bloginfo('name'); ?></title>
+    <link rel="apple-touch-icon" href="<?php bloginfo('template_url'); ?>/images/touch-icon.png" />
+    <link rel="shortcut icon" href="<?php bloginfo('template_url'); ?>/images/favicon.ico" />
+    <link rel="stylesheet" type="text/css" media="all" href="<?php bloginfo('stylesheet_url'); ?>" />
+    <!--[if lt IE 9]>
+    <meta http-equiv="Imagetoolbar" content="no" />
+    <script src="http://html5shiv.googlecode.com/svn/trunk/html5.js"></script>
+    <![endif]-->
 </head>
-
-<body <?php body_class(); ?>>
-<div id="page" class="site">
-
-	<?php get_template_part( 'template-parts/skip', 'links' );?>
-
-	<header id="masthead" class="site-header">
-		<div class="wrap">
-			<div class="site-branding">
-				<?php
-				the_custom_logo();
-				if ( is_front_page() && is_home() ) : ?>
-					<h1 class="site-title"><a href="<?php echo esc_url( home_url( '/' ) ); ?>" rel="home"><?php bloginfo( 'name' ); ?></a></h1>
-				<?php else : ?>
-					<p class="site-title"><a href="<?php echo esc_url( home_url( '/' ) ); ?>" rel="home"><?php bloginfo( 'name' ); ?></a></p>
-				<?php
-				endif;
-
-				$description = get_bloginfo( 'description', 'display' );
-				if ( $description || is_customize_preview() ) : ?>
-					<p class="site-description"><?php echo $description; /* WPCS: xss ok. */ ?></p>
-				<?php
-				endif; ?>
-			</div><!-- .site-branding -->
-
-			<?php if( has_nav_menu( 'menu-1') ) : ?>
-				<nav id="site-navigation" class="main-navigation" role="navigation">
-					<button class="menu-toggle" aria-controls="primary-menu" aria-expanded="false">
-						<?php
-						echo pacific_get_svg( array( 'icon' => 'menu' ) ); /* WPCS: xss ok. */
-						echo pacific_get_svg( array( 'icon' => 'close' ) ); /* WPCS: xss ok. */
-						?>
-						<span class="span screen-reader-text"><?php esc_html_e( 'Navigation', 'pacific' );?></span>
-					</button>
-					<?php
-						wp_nav_menu( array(
-							'theme_location' 	=> 'menu-1',
-							'menu_id'        	=> 'primary-menu',
-							'container_class' 	=> 'wrap'
-						) );
-					?>
-				</nav><!-- #site-navigation -->
-			<?php endif;?>
-		</div><!-- .wrap -->
-	</header><!-- #masthead -->
-
-	<?php get_template_part( 'template-parts/content', 'hero' );?>
-
-	<?php pacific_breadcrumb();?>
-
-	<div id="content" class="site-content">
+<body class="home">
+<div id="wrap">
+    <section id="description">
+        <h1><?php bloginfo('description'); ?></h1>
+    </section><!-- #description end -->
+    <div id="container">
+        <header id="header">
+            <h1 id="site-id">
+                <a href="<?php echo home_url('/'); ?>"><img src="<?php bloginfo('template_url'); ?>/images/header/site_id.png" alt="<?php bloginfo('name'); ?>" /></a>
+            </h1><!-- #site-id end -->
+            <div id="utility-group">
+                <nav id="utility-nav">
+                    <ul>
+                        <li><a href="#">utility-nav_1</a></li>
+                        <li><a href="#">utility-nav_2</a></li>
+                        <li><a href="#">utility-nav_3</a></li>
+                    </ul>
+                </nav><!-- #utility-nav end -->
+                <div id="header-widget-area">
+                    <aside class="widget_search">
+                        <form role="search" id="searchform">
+                            <div>
+                                <input type="text" id="s" />
+                                <input type="submit" id="searchsubmit"/>
+                            </div>
+                        </form><!-- #searchform end -->
+                    </aside><!-- .widget_search end -->
+                </div><!-- #header-widget-area end -->
+            </div><!-- #utility-group end -->
+        </header><!-- #header end -->
+        <nav id="global-nav">
+            <ul id="menu-global">
+                <li id="menu-item-home" class="menu-item current-menu-item"><a href="#">トップページ</a></li>
+                <li id="menu-item-about" class="menu-item"><a href="#">会社概要</a></li>
+                <li id="menu-item-mall" class="menu-item"><a href="#">モール開発実績</a>
+                    <ul class="sub-menu">
+                        <li class="menu-item"><a href="#">モール1</a></li>
+                        <li class="menu-item"><a href="#">モール2</a></li>
+                    </ul>
+                </li>
+                <li id="menu-item-column" class="menu-item"><a href="#">コラム</a></li>
+                <li id="menu-item-inquiry" class="menu-item"><a href="#">お問い合わせ</a></li>
+            </ul><!-- #menu-global end -->
+        </nav><!-- #global-nav end -->
+        <section id="branding">
+            <img src="<?php bloginfo('template_url'); ?>/images/top/main_image.png" width="950" height="295" alt="" />
+        </section><!-- #branding end -->
+        <section id="contents-body">
---
+++ wp-content/themes/pacific/sidebar-top.php
@@ -0,0 +1,67 @@
+<section id="sidebar">
+    <aside class="rss_link">
+        <a href="#"><img src="<?php bloginfo('template_url'); ?>/images/btn_rss_feed.png" width="250" height="28" alt="RSS" /></a>
+    </aside>
+    <div id="primary" class="widget-area">
+        <aside id="event-info" class="news-list">
+            <h1>イベント開催情報</h1>
+            <div class="info-wrap">
+                <ul>
+                    <li>
+                        <time class="entry-date" datetime="2012-01-01">entry-date</time>
+                        <h2><a href="#">event-info_title_1</a></h2>
+                        <a href="#"><img width="61" height="61" src="<?php bloginfo('template_url'); ?>/images/top/post_image.png" alt="「バンコクロイヤルガーデンフェア」開催のお知らせ" /></a>
+                        <p>テキストテキストテキストテキストテキストテキストテキストテキスト</p>
+                    </li>
+                    <li>
+                        <time class="entry-date" datetime="2012-01-01">entry-date</time>
+                        <h2><a href="#">event-info_title_2</a></h2>
+                        <a href="#"><img width="61" height="61" src="<?php bloginfo('template_url'); ?>/images/top/post_image.png" alt="「汐留 味紀行」開催のお知らせ" /></a>
+                        <p>テキストテキストテキストテキストテキストテキストテキストテキスト</p>
+                    </li>
+                </ul>
+                <span class="link-text"><a href="#">イベント開催情報一覧</a></span>
+            </div><!-- .info-wrap end -->
+        </aside><!-- #event-info end -->
+        <aside id="malls-info" class="news-list">
+            <h1>新規モール出店情報</h1>
+            <div class="info-wrap">
+                <ul>
+                    <li>
+                        <time class="entry-date" datetime="2012-01-01">entry-date</time>
+                        <h2><a href="#">malls-info_title_1</a></h2>
+                        <a href="#"><img width="61" height="61" src="<?php bloginfo('template_url'); ?>/images/top/post_image.png" alt="ホノルルに「カメハメハモール」オープン" /></a>
+                        <p>テキストテキストテキストテキストテキストテキストテキストテキスト</p>
+                    </li>
+                    <li>
+                        <time class="entry-date" datetime="2012-01-01">entry-date</time>
+                        <h2><a href="#">malls-info_title_2</a></h2>
+                        <a href="#"><img width="61" height="61" src="<?php bloginfo('template_url'); ?>/images/top/post_image.png" alt="サンフランシスコ・ノブヒル地区に「ゴールデンゲートモール」オープン" /></a>
+                        <p>テキストテキストテキストテキストテキストテキストテキストテキスト</p>
+                    </li>
+                </ul>
+                <span class="link-text"><a href="#">モール出店情報一覧</a></span>
+            </div><!-- #info-wrap end -->
+        </aside><!-- #malls-info end -->
+        <aside id="information-info" class="news-list">
+            <h1>お知らせ</h1>
+            <div class="info-wrap">
+                <ul>
+                    <li>
+                        <time class="entry-date" datetime="2012-01-01">entry-date</time>
+                        <h2><a href="#">information-info_title_1</a></h2>
+                        <a href="#"><img width="61" height="61" src="<?php bloginfo('template_url'); ?>/images/top/post_image.png" alt="人材募集のお知らせ" /></a>
+                        <p>テキストテキストテキストテキストテキストテキストテキストテキスト</p>
+                    </li>
+                    <li>
+                        <time class="entry-date" datetime="2012-01-01">entry-date</time>
+                        <h2><a href="#">information-info_title_2</a></h2>
+                        <a href="#"><img width="61" height="61" src="<?php bloginfo('template_url'); ?>/images/top/post_image.png"  alt="ホノルル支店を開設いたしました。" /></a>
+                        <p>テキストテキストテキストテキストテキストテキストテキストテキスト</p>
+                    </li>
+                </ul>
+                <span class="link-text"><a href="#">お知らせ一覧</a></span>
+            </div><!-- .info-wrap end -->
+        </aside><!-- #information-info end -->
+    </div><!-- #primary end -->
+</section><!-- #sidebar end -->
